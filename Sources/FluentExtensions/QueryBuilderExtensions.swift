880219411fe1385c2ec96af339e3ad1fd11e3fae
Brian Strobach <brian@appsaurus.io>
2019-05-07 16:17:43 -0500

    Removing ID type restriction on random query results

--- Sources/FluentExtensions/QueryBuilderExtensions.swift
+++ Sources/FluentExtensions/QueryBuilderExtensions.swift
@@ -8,45 +8,44 @@
 import Foundation
 import Fluent
 
-
-extension Model where Database: QuerySupporting, ID == Int{
-    public static func random(on conn: DatabaseConnectable) -> Future<Self?>{
+public extension Model{
+    static func random(on conn: DatabaseConnectable) -> Future<Self?>{
         return query(on: conn).random()
     }
-
-    public static func random(on conn: DatabaseConnectable, count: Int) -> Future<[Self]>{
-        return query(on: conn).random(count: count)
+    
+    static func random(on conn: DatabaseConnectable, count: Int) -> Future<[Self]>{
+        return query(on: conn).randomSlice(count: count)
     }
 }
 
-extension QueryBuilder where Result: Model, Result.ID == Int{
-	public func random() -> Future<Result?>{
-        return self.count().flatMap(to: Optional<Result>.self) { total in
-            guard total > 0 else { return self.emptyResult() }
-            return self.filter(Result.idKey == .random(in: 0..<total)).first()
-        }
-	}
+extension QueryBuilder {
 
-	public func random(count: Int) -> Future<[Result]>{
-		return self.count().flatMap(to: [Result].self) { total in
-			guard total > 0 else { return self.emptyResults()}
-			let queryRange = (0..<total).randomSubrange(count)
-			return self.range(queryRange).all()
-		}
-	}
+    func random() -> Future<Result?>{
+        return self.randomSlice(count: 1).map({ (model) -> (Result?) in
+            return model.first
+        })
+    }
+
+    func randomSlice(count: Int) -> Future<[Result]>{
+        return self.count().flatMap(to: [Result].self) { total in
+            guard total > 0 else { return self.emptyResults()}
+            let queryRange = (1...total).randomSubrange(count)
+            return self.range(queryRange).all()
+        }
+    }
 }
 
-extension QueryBuilder{
-	public func emptyResults() -> Future<[Result]> {
+public extension QueryBuilder{
+	func emptyResults() -> Future<[Result]> {
 		return self.connection.eventLoop.newSucceededFuture(result: [Result]())
 	}
-	public func emptyResult() -> Future<Result?> {
+	func emptyResult() -> Future<Result?> {
 		return self.connection.eventLoop.newSucceededFuture(result: nil)
 	}
 }
 
-extension QueryBuilder {
-	public func or(_ values: Database.QueryFilter...) -> Self {
+public extension QueryBuilder {
+	func or(_ values: Database.QueryFilter...) -> Self {
 		return group(Database.queryFilterRelationOr) { (or) in
 			for value in values{
 				or.filter(custom: value)
@@ -54,7 +53,7 @@ extension QueryBuilder {
 		}
 	}
 
-	public func and(_ values: Database.QueryFilter...) -> Self {
+	func and(_ values: Database.QueryFilter...) -> Self {
 		return group(Database.queryFilterRelationAnd) { (and) in
 			for value in values{
 				and.filter(custom: value)
