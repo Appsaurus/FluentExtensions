8da1f84eeb68fa58cb44827bdd1d1381361496b0
Brian Strobach <brian@appsaurus.io>
2019-06-06 09:27:08 -0500

    Updated dependencies and set minimum deployment target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "FluentExtensions",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         .library(
             name: "FluentExtensions",
