eb9a8f248a9c6d9ad2f367df85bb8c1bd232aac3
Brian Strobach <brian@appsaurus.io>
2021-08-10 13:50:00 -0500

    WIP vapor 4 updates

--- Sources/FluentExtensions/KeyPathExtensions.swift
+++ Sources/FluentExtensions/KeyPathExtensions.swift
@@ -5,21 +5,21 @@
 //  Created by Brian Strobach on 6/22/18.
 //
 
-//import Foundation
-//import Fluent
-//extension KeyPath where Root: Model{
+import Foundation
+import FluentKit
+extension KeyPath where Root: Model, Value: QueryableProperty{
 //	public func querySort(_ direction: Root.Database.QuerySortDirection = Root.Database.querySortDirectionAscending)-> Root.Database.QuerySort{
 //		return Root.Database.querySort(queryField, direction)
 //	}
-//	public var fluentProperty: FluentProperty{
-//		return .keyPath(self)
-//	}
+	public var fluentProperty: [FieldKey]{
+        return Root.path(for: self)
+	}
 //
 //	public var queryField: Root.Database.QueryField{
 //		return Root.Database.queryField(fluentProperty)
 //	}
-//
-//	public var propertyName: String{
-//		return fluentProperty.name
-//	}
-//}
+
+	public var propertyName: String{
+        return fluentProperty.map({$0.description}).joined(separator: ".")
+	}
+}
