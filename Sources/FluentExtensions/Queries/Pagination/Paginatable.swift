739777c4f02c5c541d744b2ee3eea4bf33749a1a
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2021-09-27 10:58:57 -0500

    Removing default page sorts.

--- Sources/FluentExtensions/Queries/Pagination/Paginatable.swift
+++ Sources/FluentExtensions/Queries/Pagination/Paginatable.swift
@@ -34,20 +34,20 @@ public extension Paginatable {
         return Pagination.Defaults.maxPageSize
     }
 }
-
-extension Paginatable where Self: Model {
-    static var defaultPageSorts: [DatabaseQuery.Sort] {
-        return [
-            .sort(idPropertyKeyPath, .descending)
-        ]
-    }
-}
-
-extension Paginatable where Self: Timestampable {
-    public static var defaultPageSorts: [DatabaseQuery.Sort] {
-        return [
-            .sort(createdAtKeyPath, .descending)
-        ]
-    }
-}
+//
+//extension Paginatable where Self: Model {
+//    static var defaultPageSorts: [DatabaseQuery.Sort] {
+//        return [
+//            .sort(idPropertyKeyPath, .descending)
+//        ]
+//    }
+//}
+//
+//extension Paginatable where Self: Timestampable {
+//    public static var defaultPageSorts: [DatabaseQuery.Sort] {
+//        return [
+//            .sort(createdAtKeyPath, .descending)
+//        ]
+//    }
+//}
 
