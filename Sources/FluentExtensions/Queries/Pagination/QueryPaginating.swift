1bfa8c2517ef6d88e61805fb26b3028ebb8fd921
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2021-09-27 13:08:17 -0500

    Variadic decoding for raw sql queries to make joined query decoding easier and more type safe.

--- Sources/FluentExtensions/Queries/Pagination/QueryPaginating.swift
+++ Sources/FluentExtensions/Queries/Pagination/QueryPaginating.swift
@@ -47,21 +47,63 @@ extension QueryPaginating where PaginatedData == SQLRow {
     }
 }
 
-extension Future where Value == Page<SQLRow> {
+public extension Future where Value == Page<SQLRow> {
 
-    public func transformingEachRow<D>(with transformer: @escaping (SQLRow) throws -> D) -> EventLoopFuture<Page<D>> {
+    func transformingEachRow<D>(with transformer: @escaping (SQLRow) throws -> D) -> EventLoopFuture<Page<D>> {
         self.flatMapThrowing {
             try $0.transformDatum(with: transformer)
         }
     }
 
-    public func transformingRows<D>(with transformer: @escaping ([SQLRow]) throws -> [D]) -> EventLoopFuture<Page<D>> {
+    func transformingRows<D>(with transformer: @escaping ([SQLRow]) throws -> [D]) -> EventLoopFuture<Page<D>> {
         self.flatMapThrowing {
             try $0.transformData(with: transformer)
         }
     }
 
-    public func decodingRows<D>(as type: D.Type = D.self) -> EventLoopFuture<Page<D>> where D: Decodable {
+    func decodingRows<D>(as type: D.Type = D.self) -> EventLoopFuture<Page<D>> where D: Decodable {
         transformingEachRow(with: { try $0.decode(model: type) })
+    }
+}
+
+// Variadic decoding
+
+public extension Future where Value == Page<SQLRow> {
+    func decodingRows<M1: Codable, M2: Codable>(as model: M1.Type = M1.self,
+                                                _ model2: M2.Type = M2.self) -> Future<Page<(M1, M2)>> {
+        return transformingEachRow(with: {try $0.decode(model, model2)})
+    }
+
+    func decodingRows<M1: Codable, M2: Codable, M3: Codable>(as model: M1.Type = M1.self,
+                                                             _ model2: M2.Type = M2.self,
+                                                             _ model3: M3.Type = M3.self) -> Future<Page<(M1, M2, M3)>> {
+        return transformingEachRow(with: {try $0.decode(model, model2, model3)})
+    }
+
+    func decodingRows<M1: Codable, M2: Codable, M3: Codable, M4: Codable>(as model: M1.Type = M1.self,
+                                                                          _ model2: M2.Type = M2.self,
+                                                                          _ model3: M3.Type = M3.self,
+                                                                          _ model4: M4.Type = M4.self) -> Future<Page<(M1, M2, M3, M4)>> {
+        return transformingEachRow(with: {try $0.decode(model, model2, model3, model4)})
+    }
+}
+
+
+public extension SQLRow {
+    func decode<M1: Codable, M2: Codable>(_ model: M1.Type = M1.self, _ model2: M2.Type = M2.self) throws -> (M1, M2) {
+        return (try decode(model: model), try decode(model: model2))
+    }
+
+    func decode<M1: Codable, M2: Codable, M3: Codable>(_ model: M1.Type = M1.self,
+                                                             _ model2: M2.Type = M2.self,
+                                                             _ model3: M3.Type = M3.self) throws -> (M1, M2, M3) {
+        return (try decode(model: model), try decode(model: model2), try decode(model: model3))
+    }
+
+    func decode<M1: Codable, M2: Codable, M3: Codable, M4: Codable>(_ model: M1.Type = M1.self,
+                                                                          _ model2: M2.Type = M2.self,
+                                                                          _ model3: M3.Type = M3.self,
+                                                                          _ model4: M4.Type = M4.self) throws -> (M1, M2, M3, M4) {
+        return (try decode(model: model), try decode(model: model2), try decode(model: model3), try decode(model: model4))
     }
 }
