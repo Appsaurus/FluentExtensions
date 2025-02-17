fb21d786b32fdea29e378cae32e36e87c7e05fb6
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2025-02-17 12:22:49 -0600

    Add async route handling methods to RoutesBuilder extension
    Introduced asynchronous route handling methods to RoutesBuilder, allowing
    for async request processing with various HTTP methods. This enhances the
    flexibility and performance of the routing system, supporting async/await
    patterns. Additionally, made some utility methods public for broader access.

--- Sources/FluentExtensions/Queries/DatabaseQuery++.swift
+++ Sources/FluentExtensions/Queries/DatabaseQuery++.swift
@@ -7,7 +7,7 @@
 
 import Fluent
 
-extension DatabaseQuery.Filter.Method {
+public extension DatabaseQuery.Filter.Method {
     static var inSubSet: Self {
         .subset(inverse: false)
     }
@@ -18,9 +18,16 @@ extension DatabaseQuery.Filter.Method {
 }
 
 
-extension DatabaseQuery.Sort {
-    static func sort<M: Model, V: QueryableProperty>(_ keyPath: KeyPath<M, V>, _
-                                                        direction: DatabaseQuery.Sort.Direction = .descending) -> DatabaseQuery.Sort {
+public extension DatabaseQuery.Sort {
+    static func sort<M: Model, V: QueryableProperty>(_ keyPath: KeyPath<M, V>, _ direction: DatabaseQuery.Sort.Direction) -> DatabaseQuery.Sort {
         DatabaseQuery.Sort.sort(keyPath.databaseQueryField, direction)
+    }
+    
+    static func descending<M: Model, V: QueryableProperty>(_ keyPath: KeyPath<M, V>) -> DatabaseQuery.Sort {
+        sort(keyPath, .descending)
+    }
+    
+    static func ascending<M: Model, V: QueryableProperty>(_ keyPath: KeyPath<M, V>) -> DatabaseQuery.Sort {
+        sort(keyPath, .ascending)
     }
 }
