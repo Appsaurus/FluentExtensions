8fc8c670b4382ee63caa014a1094d7e33d63fa5c
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2025-04-17 16:25:19 -0500

    Simplifying doc generation.

--- generate-docs.sh
+++ generate-docs.sh
@@ -3,7 +3,7 @@ swift package \
 --allow-writing-to-directory ./docs \
 generate-documentation \
 --target FluentExtensions \
---output-path ./docs \
+--output-path 'docs' \
 --transform-for-static-hosting \
 --hosting-base-path FluentExtensions \
 --disable-indexing
