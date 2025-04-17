1071a6001f0c3b9c9f42b96dd2a11cab18267d0c
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2025-04-17 14:11:35 -0500

    Refactor GitHub Pages deployment workflow
    - Consolidate build and deployment jobs to simplify CI process
    - Add static hosting configuration parameters to documentation generator
    - Remove redundant deployment environment setup

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-swift package --allow-writing-to-directory ./docs \
-    generate-documentation --target FluentExtensions --output-path ./docs
+swift package \
+--allow-writing-to-directory ./docs \
+generate-documentation \
+--target FluentExtensions
+--output-path ./docs
+--transform-for-static-hosting \
+--hosting-base-path fluentextensions
